56ec99fbcec690bdf0bbe44373b3cf4160200bdc
Cory Binnersley <corybinnersley@gmail.com>
2019-08-17 23:19:11 -0700

    Added batch accumulation

--- keras_radam/optimizers.py
+++ keras_radam/optimizers.py
@@ -2,11 +2,11 @@ from .backend import keras
 from .backend import backend as K
 
 
-__all__ = ['RAdam']
+__all__ = ["RAdamAccumulator"]
 
 
-class RAdam(keras.optimizers.Optimizer):
-    """RAdam optimizer.
+class RAdamAccumulator(keras.optimizers.Optimizer):
+    """RAdamAccumulator optimizer.
 
     # Arguments
         lr: float >= 0. Learning rate.
@@ -27,20 +27,35 @@ class RAdam(keras.optimizers.Optimizer):
         - [On The Variance Of The Adaptive Learning Rate And Beyond](https://arxiv.org/pdf/1908.03265v1.pdf)
     """
 
-    def __init__(self, lr=0.001, beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., weight_decay=0., amsgrad=False,
-                 total_steps=0, warmup_proportion=0.1, min_lr=0., **kwargs):
-        super(RAdam, self).__init__(**kwargs)
+    def __init__(
+        self,
+        lr=0.001,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=None,
+        decay=0.0,
+        weight_decay=0.0,
+        amsgrad=False,
+        total_steps=0,
+        warmup_proportion=0.1,
+        min_lr=0.0,
+        accum_iters=1,
+        **kwargs
+    ):
+        super(RAdamAccumulator, self).__init__(**kwargs)
         with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            self.lr = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-            self.weight_decay = K.variable(weight_decay, name='weight_decay')
-            self.total_steps = K.variable(total_steps, name='total_steps')
-            self.warmup_proportion = K.variable(warmup_proportion, name='warmup_proportion')
-            self.min_lr = K.variable(lr, name='min_lr')
+            self.iterations = K.variable(0, dtype="int64", name="iterations")
+            self.lr = K.variable(lr, name="lr")
+            self.beta_1 = K.variable(beta_1, name="beta_1")
+            self.beta_2 = K.variable(beta_2, name="beta_2")
+            self.decay = K.variable(decay, name="decay")
+            self.weight_decay = K.variable(weight_decay, name="weight_decay")
+            self.total_steps = K.variable(total_steps, name="total_steps")
+            self.warmup_proportion = K.variable(
+                warmup_proportion, name="warmup_proportion"
+            )
+            self.min_lr = K.variable(lr, name="min_lr")
+            self.accum_iters = K.variable(lr, name="accum_iters")
         if epsilon is None:
             epsilon = K.epsilon()
         self.epsilon = epsilon
@@ -56,7 +71,9 @@ class RAdam(keras.optimizers.Optimizer):
         lr = self.lr
 
         if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations, K.dtype(self.decay))))
+            lr = lr * (
+                1.0 / (1.0 + self.decay * K.cast(self.iterations, K.dtype(self.decay)))
+            )
 
         t = K.cast(self.iterations, K.floatx()) + 1
 
@@ -69,13 +86,29 @@ class RAdam(keras.optimizers.Optimizer):
                 lr * (1.0 - K.minimum(t, decay_steps) / decay_steps),
             )
 
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='m_' + str(i)) for (i, p) in enumerate(params)]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='v_' + str(i)) for (i, p) in enumerate(params)]
+        update_switch = K.equal((self.iterations + 1) % self.accum_iters, 0)
+        update_switch = K.cast(update_switch, K.floatx())
+
+        ms = [
+            K.zeros(K.int_shape(p), dtype=K.dtype(p), name="m_" + str(i))
+            for (i, p) in enumerate(params)
+        ]
+        vs = [
+            K.zeros(K.int_shape(p), dtype=K.dtype(p), name="v_" + str(i))
+            for (i, p) in enumerate(params)
+        ]
+        gs = [
+            K.zeros(K.int_shape(p), dtype=K.dtype(p), name="g_" + str(i))
+            for (i, p) in enumerate(params)
+        ]
 
         if self.amsgrad:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='vhat_' + str(i)) for (i, p) in enumerate(params)]
+            vhats = [
+                K.zeros(K.int_shape(p), dtype=K.dtype(p), name="vhat_" + str(i))
+                for (i, p) in enumerate(params)
+            ]
         else:
-            vhats = [K.zeros(1, name='vhat_' + str(i)) for i in range(len(params))]
+            vhats = [K.zeros(1, name="vhat_" + str(i)) for i in range(len(params))]
 
         self.weights = [self.iterations] + ms + vs + vhats
 
@@ -85,21 +118,32 @@ class RAdam(keras.optimizers.Optimizer):
         sma_inf = 2.0 / (1.0 - self.beta_2) - 1.0
         sma_t = sma_inf - 2.0 * t * beta_2_t / (1.0 - beta_2_t)
 
-        for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
+        for p, g, m, v, vhat, tg in zip(params, grads, ms, vs, vhats, gs):
+
+            sum_grad = tg + g
+            avg_grad = sum_grad / self.accum_iters_float
+
+            m_t = (self.beta_1 * m) + (1.0 - self.beta_1) * avg_grad
+            v_t = (self.beta_2 * v) + (1.0 - self.beta_2) * K.square(avg_grad)
 
             m_corr_t = m_t / (1.0 - beta_1_t)
             if self.amsgrad:
                 vhat_t = K.maximum(vhat, v_t)
                 v_corr_t = K.sqrt(vhat_t / (1.0 - beta_2_t) + self.epsilon)
-                self.updates.append(K.update(vhat, vhat_t))
+                self.updates.append(
+                    K.update(vhat, (1 - update_switch) * vhat + update_switch * vhat_t)
+                )
             else:
                 v_corr_t = K.sqrt(v_t / (1.0 - beta_2_t) + self.epsilon)
 
-            r_t = K.sqrt((sma_t - 4.0) / (sma_inf - 4.0) *
-                         (sma_t - 2.0) / (sma_inf - 2.0) *
-                         sma_inf / sma_t)
+            r_t = K.sqrt(
+                (sma_t - 4.0)
+                / (sma_inf - 4.0)
+                * (sma_t - 2.0)
+                / (sma_inf - 2.0)
+                * sma_inf
+                / sma_t
+            )
 
             p_t = K.switch(sma_t > 5, r_t * m_corr_t / v_corr_t, m_corr_t)
 
@@ -108,29 +152,37 @@ class RAdam(keras.optimizers.Optimizer):
 
             p_t = p - lr * p_t
 
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
+            self.updates.append(
+                K.update(m, (1 - update_switch) * m + update_switch * m_t)
+            )
+            self.updates.append(
+                K.update(v, (1 - update_switch) * v + update_switch * v_t)
+            )
+            self.updates.append(K.update(tg, (1 - update_switch) * sum_grad))
             new_p = p_t
 
             # Apply constraints.
-            if getattr(p, 'constraint', None) is not None:
+            if getattr(p, "constraint", None) is not None:
                 new_p = p.constraint(new_p)
 
-            self.updates.append(K.update(p, new_p))
+            self.updates.append(
+                K.update(p, (1 - update_switch) * p + update_switch * new_p)
+            )
         return self.updates
 
     def get_config(self):
         config = {
-            'lr': float(K.get_value(self.lr)),
-            'beta_1': float(K.get_value(self.beta_1)),
-            'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
-            'weight_decay': float(K.get_value(self.weight_decay)),
-            'epsilon': self.epsilon,
-            'amsgrad': self.amsgrad,
-            'total_steps': float(K.get_value(self.total_steps)),
-            'warmup_proportion': float(K.get_value(self.warmup_proportion)),
-            'min_lr': float(K.get_value(self.min_lr)),
+            "lr": float(K.get_value(self.lr)),
+            "beta_1": float(K.get_value(self.beta_1)),
+            "beta_2": float(K.get_value(self.beta_2)),
+            "decay": float(K.get_value(self.decay)),
+            "weight_decay": float(K.get_value(self.weight_decay)),
+            "epsilon": self.epsilon,
+            "amsgrad": self.amsgrad,
+            "total_steps": float(K.get_value(self.total_steps)),
+            "warmup_proportion": float(K.get_value(self.warmup_proportion)),
+            "min_lr": float(K.get_value(self.min_lr)),
+            "accum_iters": float(K.get_value(self.accum_iters)),
         }
-        base_config = super(RAdam, self).get_config()
+        base_config = super(RAdamAccumulator, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
